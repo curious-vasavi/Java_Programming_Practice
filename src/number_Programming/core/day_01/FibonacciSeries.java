aa3fbfca760872a71806960a2203665498d52030
curious-vasavi <mangarajuvasavi@gmail.com>
2025-12-15 20:35:53 +0530

    Update FibonacciSeries.java

--- src/number_Programming/core/day_01/FibonacciSeries.java
+++ src/number_Programming/core/day_01/FibonacciSeries.java
@@ -1,8 +1,6 @@
 package number_Programming.core.day_01;
 import java.util.Scanner;
 //12.write a program to produce the fibonacci series
-public class FibonacciSeries 
-{
 public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
 	System.out.println("Enter n value : ");
